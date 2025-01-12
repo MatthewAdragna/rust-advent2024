3a86decb21e2accb950b16627adfb85079c1fba9
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-11 23:04:45 -0500

    done-ish. For some reason my command line input is tainted

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,34 +1,39 @@
 use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the number");
-    println!("Please input your number");
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read Line");
-    
-    let guess:i32 = guess.trim().parse()
-        .expect("The inputted value was not recognized as a number: Please try next time!");
-    
-
     let secret_num:i32 = rand::thread_rng().gen_range(1..=100);
-
-    println!("You guessed {} !", guess);
     println!("The secret_num was {secret_num}");
-
-    if secret_num == guess
-    { 
-        println!("You got it right!");}
-    else
-    { 
-        println!("You got it wrong :C .");
-    }
     
 
+    loop {
+        println!("Please input your number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read Line");
+        
+        let guess:i32 = match guess.trim().parse()
+        {
+            Ok(number) => number,
+            Err(_) => {println!("Unable to convert input to i32");continue;},
+
+        };    
+    
+        println!("You guessed {} !", guess);
+    
+        match guess.cmp(&secret_num){
+            Ordering::Less => println!("Too little!"),
+            Ordering::Greater => println!("Too much!"),
+            Ordering::Equal =>{ 
+                println!("You got it!");
+                break;
+            },
+            } 
+        }
 
 
 }
