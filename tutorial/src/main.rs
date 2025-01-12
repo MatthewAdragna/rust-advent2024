27e49334336af4b1da295820f9fb08c000adfccd
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-11 23:28:29 -0500

    Fixed my issue - read_line appends to the referred string :)

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -4,27 +4,33 @@ use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the number");
-    let mut guess = String::new();
 
     let secret_num:i32 = rand::thread_rng().gen_range(1..=100);
-    println!("The secret_num was {secret_num}");
     
 
     loop {
+
+        let mut guess = String::new();
+
         println!("Please input your number");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read Line");
-        
+    
         let guess:i32 = match guess.trim().parse()
         {
-            Ok(number) => number,
-            Err(_) => {println!("Unable to convert input to i32");continue;},
+            Ok(number) => {
+                println!("You guessed {number}");
+                number
+            },
+            Err(_) => {
+                println!("Unable to convert input to i32");
+                continue;
+            },
 
         };    
     
-        println!("You guessed {} !", guess);
-    
+        // Handling <=> conditions  
         match guess.cmp(&secret_num){
             Ordering::Less => println!("Too little!"),
             Ordering::Greater => println!("Too much!"),
