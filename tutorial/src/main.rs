aae0310c6e595f7f81cfa75df798b7df0dd0ae43
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-12 19:00:26 -0500

    Continuing to learn: Data Types, Functions so far. Getting in the commit for the day

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -4,6 +4,9 @@ use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the number");
+    
+
+    let tupes: (i32,f64,char) = (55,21.0,'a');
 
     let secret_num:i32 = rand::thread_rng().gen_range(1..=100);
     
