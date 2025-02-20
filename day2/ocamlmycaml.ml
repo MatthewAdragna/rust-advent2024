f93252b51b9359349f0970416049db7cf13c5610
MatthewAdragna <matthewjadragna@gmail.com>
2025-02-20 02:55:06 -0500

    Im too tired - I reorganized my functions in a more sleek functional way, but im back to the same problem of how do i do this cleanly

--- day2/ocamlmycaml.ml
+++ day2/ocamlmycaml.ml
@@ -1,73 +1,97 @@
 
+let map = List.map 
+let identity x = x 
+let p = print_endline
+let pf = Printf.printf
 let lines f= 
   let channel = In_channel.with_open_bin f In_channel.input_all
   in String.split_on_char '\n' channel 
   |> List.filter (fun x ->(String.length (String.trim x) ) >= 1) 
+
 let printStrArr arrIn = 
   for i = 0 to (Array.length arrIn)-1 do
     Printf.printf "[%i :%s]" i arrIn.(i)
   done
+
 let default_file = "input"
+
 let file = if Array.length Sys.argv >=2  then Sys.argv.(1) else default_file
 
+let lToStr ( l:int list )= map ( string_of_int ) l |> List.fold_left ( ^ ) ""
 
-let lineToIntList l = let lineList = String.split_on_char ' ' l in
-  let rec aux listOut lineList =
-    match lineList with
-    | x :: rest -> if x = "" then aux ([(int_of_string x)] @ listOut) rest else aux listOut rest
-    | [] -> listOut 
-    in aux [] lineList
+let printList l = ( "|[Line]:"^(lToStr l) ^ "|") |> print_endline
+
+let intlist_of_str strIn = String.split_on_char ' ' strIn |> map int_of_string 
   
-let linesToIntIntList l = 
-  let rec aux totalList l =
-    match l with
-      | [] -> totalList
-      | s :: rest -> aux ((lineToIntList s) @ totalList) rest
-  in aux [] l
-let rec badAdventure listIn =
-   match listIn with
-  |a :: b :: rest -> 
-  let diff = a - b in
-    if diff > 0 || (abs diff) > 2 then 0 else badAdventure ([b] @  rest)
-  | _  -> 1 
+let linesToIntList2 (l:  string list) = map intlist_of_str l
+
+let diff = fun x y -> ( - ) x y
+let diffTup (x,y) = diff x y
+
+let rec pairs listIn =
+    match listIn with 
+    | a :: (b :: _ as rest) ->  (a,b) :: pairs rest
+    | _ -> []
+let diffs l = pairs l |> map diffTup 
+let is_dec listIn = List.for_all (fun x -> x < 0) listIn 
+let is_inc listIn = List.for_all (fun x -> x > 0) listIn 
+let is_bounded  listIn = List.for_all(fun x -> let ab = abs x in ab > 0 && ab < 4) listIn
+let safe listIn = is_bounded listIn && (is_inc listIn || is_dec listIn)
+let lineIsSafe1 lineIn = intlist_of_str lineIn |> diffs |> safe
+let linesAreSafe1 linesIn = map lineIsSafe1 linesIn
+let countLines listIn = map (fun x -> match x with | true -> 1 | false -> 0) listIn |> List.fold_left ( + ) 0
+let part1  input = file |> lines |> linesAreSafe1 |> countLines |> string_of_int
+
+type sign = 
+  | Neg 
+  | Zero 
+  | Starting
+  | Pos 
+let dts x = if x = 0 then Zero else if x > 0 then Pos else Neg
+
+let sod a b = dts (diff a b)
+let des a b sign = sod a b |> ( = ) sign
+
+let lineIsSafe2 linesIn = 
+  let linesIn = linesIn |> intlist_of_str in
+  let a :: b :: c :: rest = linesIn in 
+  let defSign = 
+      let ab = sod a b in
+      let bc = sod b c in
+      let ac = sod a c in 
+  if ab = bc && bc == ac then ab
+else if ab = ac && 
+      
 
 
-let rec goodAdventure listIn =
-   match listIn with
-  |a :: b :: rest -> 
-  let diff = a - b in
-    if diff < 0 || (abs diff) > 2 then 0 else goodAdventure ([b] @  rest)
-  | _  -> 1 
+  let rec aux (skip:int) ( sign:sign ) list =
 
-
-let safetyDance x =  
-  let a,b,rest = match x with
-  |a :: b :: rest -> (a,b,rest)
-  |_ -> (-1,-1,[-1])
-
-  in
-  let diff = a - b in 
-  if abs diff > 2  then
-    0
-  else if diff < 0 then
-    badAdventure ([b] @ rest) 
-  else
-    goodAdventure ([b] @ rest)
-
-let getSafety l =
-  let rec aux lIn lOut = 
-    match lIn with 
-    | x :: rest -> aux (rest) ([(safetyDance x)] @ lOut)
-    | [] -> lOut
-  in 
-    aux l []
-
- 
-
-
-
-
-let () = 
-  let linesIn = lines file in
+    match list with 
+    | [] -> true
+    | a :: [] -> true
+    | a :: b :: rest -> 
+      if skip = 0 then true 
+      else if des a b sign then true else false 
+    | a :: b :: c :: rest ->  
+    let ab = sod a b in
+      if sign <> ab  then
+        
   
-  let safeLevels = List.fold_left (+) 0 (getSafety (linesIn)) in in print_endline (string_of_int safeLevels);
+
+
+    
+        
+
+
+
+let linesAreSafe2 linesIn = map lineIsSafe2 linesIn
+
+
+
+let part2 input = file |> lines |> identity
+
+let () = p "Part 1:";p (part1 file);
+
+
+
+  
